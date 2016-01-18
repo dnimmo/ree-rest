3f2769307a23e098e1d992581100f8d8474a420e
Jonnyfromnothing <madebyjonny@gmail.com>
2016-01-18 14:39:10 +0000

    removed ecommerce from master

--- dev/endpoints/products/index.js
+++
@@ -1,8 +0,0 @@
-import basicRoute from '../../setup/basicRoute.js';
-import {Schema, Products} from './model.js';
-import {config} from '../../setup/config.js';
-
-var products = Object.create(basicRoute);
-products = Object.assign(products, {route: 'products', db: Products}, config);
-
-export default products;
